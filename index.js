8f0db6271ebee110bf12ec64910e227ff97e9c82
root <jpadkins@smu.edu>
2015-03-18 23:12:07 -0500

    Timers and add menu item working!

--- index.js
+++ index.js
@@ -2,63 +2,88 @@
  * OrderLite Prototype
 */
 
+// number of timers
+var iTimers = 0;
+
 // this array will have its values changed to match order names
-var sButtons = ["row 1 column 1",
-                "row 1 column 2",
-                "row 1 column 3",
-                "row 2 column 1",
-                "row 2 column 2",
-                "row 2 column 3",
-                "row 3 column 1",
-                "row 3 column 2",
-                "row 3 column 3"]
+var sButtons = ["r1c1 default", "r1c2 default", "r1c3 default",
+                "r2c1 default", "r2c2 default", "r2c3 default",
+                "r3c1 default", "r3c2 default", "r3c3 default"]
+
+// print names of orders in their boxes at startup
+$(document).ready(function(){
+  printOrders();
+});
+
+// print names of order in their boxes
+function printOrders() {
+  for (var i = 0; i < 9; i++) {
+    $("#o" + i + " h3").text(sButtons[i]);
+  }
+}
 
 // function to add a timer based on button index;
 function addTimer(iButton) {
-  $("#timers").append("<div id=\"tOrder\"><h3>" + sButtons[iButton] + "</div>");
+  $("#timers").append("<div id=\"tOrder\"><h3>" + sButtons[iButton] + ", Timer: <span id=\"timer" + iTimers + "\"></span></div>");
+
+  var d = new Date();
+  $('#timer' + iTimers).tinyTimer({ from: d, format: '%0m:%0s' });
+  iTimers = iTimers + 1;
 }
+
+// prevents submit from reloading page
+$("form").submit(function(e) {
+    e.preventDefault();
+});
+
+// addOrder button listener
+$("#addOrder").click(function() {
+  sButtons[$("#oPos").val()] = $("#oName").val();
+  printOrders();
+})
 
 // close listener for dynamic html elements
 $("body").on("click", "#tOrder", function() {
   $(this).remove();
+  iTimers = iTimers - 1;
 });
 
 /* First Row Of Order Buttons */
 // Row 1 Column 1
-$("#r1c1").click(function() {
+$("#o0").click(function() {
   addTimer(0);
 });
 // Row 1 Column 2
-$("#r1c2").click(function() {
+$("#o1").click(function() {
   addTimer(1);
 });
 // Row 1 Column 3
-$("#r1c3").click(function() {
+$("#o2").click(function() {
   addTimer(2);
 });
 /* Second Row Of Order Buttons */
 // Row 2 Column 1
-$("#r2c1").click(function() {
+$("#o3").click(function() {
   addTimer(3);
 });
 // Row 2 Column 2
-$("#r2c2").click(function() {
+$("#o4").click(function() {
   addTimer(4);
 });
 // Row 2 Column 3
-$("#r2c3").click(function() {
+$("#o5").click(function() {
   addTimer(5);
 });
 /* Third Row Of Order Buttons */
 // Row 3 Column 1
-$("#r3c1").click(function() {
+$("#o6").click(function() {
   addTimer(6);
 });
 // Row 3 Column 2
-$("#r3c2").click(function() {
+$("#o7").click(function() {
   addTimer(7);
 });
 // Row 3 Column 3
-$("#r3c3").click(function() {
+$("#o8").click(function() {
   addTimer(8);
 });
