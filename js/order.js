0754cad1749b1f56cc84f404951e624c8dbb0809
Nariana <nsands@smu.edu>
2015-04-20 22:40:53 -0500

    added images successfully, small overflow, no pic 2

--- js/order.js
+++ js/order.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
-	load();
+    load();
 });
 
 function load() {
-	loadOptions();
-	//addListeners();
+    loadOptions();
+    //addListeners();
 }
 
 function loadOptions(){
     // Get data from file as JSON
     $.getJSON('json/Recipes.json', function (data) {
 
-    	var ingr = "";
-    	if (data.length === 0) 
+        if (data.length === 0) 
             $("#order-list").append("<div class='resultDiv'><p id='no-result'>No results</p></div>");
 
         else {
             $.each(data.Recipes, function (key, recipe) {
+                var ingr = "";
                 if($("#recipe"+recipe.recipeID).has("recipeID").length === 0){
-                    var add = "<div id='recipe"+recipe.recipeID+"' class='resultDiv'><ul class='resultList'><h5 class='name'>"+recipe.recipeName+"</h5></ul></div>";
+                    var add = "<div id='recipe"+recipe.recipeID+"' class='resultDiv'><img class='orderPicture' src='img/recipes/"+recipe.recipeID+".jpeg' id='picture"+recipe.recipeID+"' alt='picture"+recipe.recipeID+"'><ul class='resultList'></ul></div>";
 
                     $("#order-list").append(add);
 
@@ -28,20 +28,23 @@ function loadOptions(){
                     });
 
                     ingr = ingr.replace(/,\s*$/, "");
-                    $("#recipe"+recipe.recipeID+">ul").append("<li class='.ingredientList'>Ingredients: "+ingr+"</li><li class='.price'>Price: "+recipe.price.toFixed(2)+"</li>");
-    			// opt += '<ul class ="recipes-ul" style="list-style-type:none"><li><button class="btn btn-default btn-lg"' +
-    			// '"value="' + recipe.recipeName +
-    			// '">' + recipe.recipeName +
-    			// '</button></li><li><p>Ingredients: ';
-    			// $.each(recipe.ingredients, function(key, ingredient) {
-    			// 	opt += ingredient + ', ';			
-    			// });
-    			// opt = opt.replace(/,\s*$/, "");
-    			// opt += '</p></li><li><p>Price: $' +
-    			// recipe.price.toFixed(2) +
-    			// '</p></li></ul>';
-                //$("#order-list").html(opt);
+                    $("#recipe"+recipe.recipeID+">ul").append("<li><h5 class='name'><b>"+recipe.recipeName+"</b></h5></li><li class='description'>\""+recipe.description+"\"</li><li class='ingredientList'><b>Ingredients:</b> "+ingr+"</li><li class='.price'><b>Price:</b> $"+recipe.price.toFixed(2)+"</li>");
+
                 }
+            });
+
+            var divs = $(".resultDiv");
+            var maxHeight = 0;
+
+            $.each(divs, function(){
+                var thisHeight = $(this).height();
+                if ( thisHeight > maxHeight){
+                  maxHeight = thisHeight;
+                }
+            });
+
+            $.each(divs, function(){
+                $(this).height(maxHeight);
             });
         }
     });
